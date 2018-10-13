e91e354c50b2950c7074017073b0b1eb2a56fc63
Fajran Iman Rusadi <fajran@gmail.com>
2018-10-13 06:26:37 +0200

    Add SecretType to mimic v1.Secret

--- pkg/apis/kuncen/v1alpha1/types.go
+++ pkg/apis/kuncen/v1alpha1/types.go
@@ -17,23 +17,24 @@ limitations under the License.
 package v1alpha1
 
 import (
+	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// Foo is a specification for a Foo resource
 type EncryptedSecret struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Data map[string]string `json:"data"`
+	Data map[string]string `json:"data,omitempty"`
+
+	Type v1.SecretType `json:"type,omitempty"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// FooList is a list of Foo resources
 type EncryptedSecretList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata"`
